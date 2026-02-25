6c92979ebbb80b708f20132f3d1a5acb606dcd6b
NatashyaPeddle <natashya.peddle@gmail.com>
2026-02-25 12:15:09 -0500

    Level Designed / Added + Assets Added

---
+++ Midterm217/Source/Midterm217/Midterm217.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Midterm217.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Midterm217, "Midterm217" );
